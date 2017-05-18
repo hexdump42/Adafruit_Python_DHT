b6554b0b8d161001673fc5d56ac2d2b5a280b8e8
Python3pkg <raliclo@gmail.com>
2017-05-17 22:16:27 -0700

    Convert to python3

--- Adafruit_DHT/Test.py
+++ Adafruit_DHT/Test.py
@@ -18,7 +18,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import common
+from . import common
 import Test_Driver as driver
 
 def read(sensor, pin):
